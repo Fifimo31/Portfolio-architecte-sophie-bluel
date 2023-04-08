1af6ddb5a6b59451cd07d2dca01714a59a950e31
Hawa Sall <sallhawa30@yahoo.fr>
2023-04-08 18:27:32 +0200

    fitrage des images

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -20,3 +20,62 @@ fetch("http://localhost:5678/api/works")
       gallery.insertAdjacentHTML('beforeend',img)
     }
   }
+  
+const filterData = (category, data) => {
+  return data.filter((item) => item.category === category);
+};
+
+const allBtn = document.querySelector(".btn button");
+const objetBtn = document.querySelector(".btn .objet");
+const appartementBtn = document.querySelector(".btn .appartement");
+const hrBtn = document.querySelector(".btn .hotelRestau");
+
+allBtn.addEventListener("click", () => {
+  fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      console.log(response.ok);
+      return response.json();
+    })
+    .then((data) => {
+      displayData(data);
+    });
+});
+
+
+objetBtn.addEventListener("click", () => {
+  fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      console.log(response.ok);
+      return response.json();
+    })
+    .then((data) => {
+      const filteredData = filterData("Objets", data);
+      displayData(filteredData);
+    });
+});
+console.log(objetBtn)
+
+appartementBtn.addEventListener("click", () => {
+  fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      console.log(response.ok);
+      return response.json();
+    })
+    .then((data) => {
+      const filteredData = filterData("Appartement", data);
+      displayData(filteredData);
+    });
+});
+
+hrBtn.addEventListener("click", () => {
+  fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      console.log(response.ok);
+      return response.json();
+    })
+    .then((data) => {
+      const filteredData = filterData("Hôtel & restaurants", data);
+      displayData(filteredData);
+    });
+});
+
