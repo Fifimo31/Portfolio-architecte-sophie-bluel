50fdd6027acdb35219e26e75d71b714257f28662
Hawa Sall <sallhawa30@yahoo.fr>
2023-04-09 21:32:33 +0200

    Filtre 2

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -10,72 +10,53 @@ fetch("http://localhost:5678/api/works")
   const displayData = (data) => {
     const gallery = document.querySelector("#portfolio .gallery")
     for  ( let item of data){
-      console.log(item)
+      console.log(item.category)
       const img = 
       `<figure>
       <img src="${item.imageUrl}" alt="Abajour Tahina">
       <figcaption>${item.title}</figcaption>
       </figure>`;
-
       gallery.insertAdjacentHTML('beforeend',img)
     }
   }
   
-const filterData = (category, data) => {
-  return data.filter((item) => item.category === category);
-};
 
 const allBtn = document.querySelector(".btn button");
 const objetBtn = document.querySelector(".btn .objet");
 const appartementBtn = document.querySelector(".btn .appartement");
 const hrBtn = document.querySelector(".btn .hotelRestau");
 
+const monSet = new Set();
+monSet.add(1);
+monSet.add(2);
+monSet.add(3);
+
+const objet = {id: 1};
+const appartements = {id: 2};
+const hotelRestau = {id: 3};
+monSet.add(objet);
+monSet.add(appartements);
+monSet.add(hotelRestau);
+console.log(monSet)
+for (let item of monSet) console.log(item);
+
 allBtn.addEventListener("click", () => {
-  fetch("http://localhost:5678/api/works")
-    .then((response) => {
-      console.log(response.ok);
-      return response.json();
-    })
-    .then((data) => {
-      displayData(data);
-    });
+  
 });
 
 
 objetBtn.addEventListener("click", () => {
-  fetch("http://localhost:5678/api/works")
-    .then((response) => {
-      console.log(response.ok);
-      return response.json();
-    })
-    .then((data) => {
-      const filteredData = filterData("Objets", data);
-      displayData(filteredData);
-    });
+
 });
 console.log(objetBtn)
 
 appartementBtn.addEventListener("click", () => {
-  fetch("http://localhost:5678/api/works")
-    .then((response) => {
-      console.log(response.ok);
-      return response.json();
-    })
-    .then((data) => {
-      const filteredData = filterData("Appartement", data);
-      displayData(filteredData);
-    });
+  
 });
 
 hrBtn.addEventListener("click", () => {
-  fetch("http://localhost:5678/api/works")
-    .then((response) => {
-      console.log(response.ok);
-      return response.json();
-    })
-    .then((data) => {
-      const filteredData = filterData("Hôtel & restaurants", data);
-      displayData(filteredData);
-    });
+
 });
+
+
 
