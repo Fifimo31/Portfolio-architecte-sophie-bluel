a8eb66acb57bc9dce6ccf000f80ad9e349895993
Hawa Sall <sallhawa30@yahoo.fr>
2023-04-10 12:06:47 +0200

    affichage des boutons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -10,7 +10,7 @@ fetch("http://localhost:5678/api/works")
   const displayData = (data) => {
     const gallery = document.querySelector("#portfolio .gallery")
     for  ( let item of data){
-      console.log(item.category)
+      console.log(item)
       const img = 
       `<figure>
       <img src="${item.imageUrl}" alt="Abajour Tahina">
@@ -20,43 +20,26 @@ fetch("http://localhost:5678/api/works")
     }
   }
   
-
-const allBtn = document.querySelector(".btn button");
-const objetBtn = document.querySelector(".btn .objet");
-const appartementBtn = document.querySelector(".btn .appartement");
-const hrBtn = document.querySelector(".btn .hotelRestau");
-
-const monSet = new Set();
-monSet.add(1);
-monSet.add(2);
-monSet.add(3);
-
-const objet = {id: 1};
-const appartements = {id: 2};
-const hotelRestau = {id: 3};
-monSet.add(objet);
-monSet.add(appartements);
-monSet.add(hotelRestau);
-console.log(monSet)
-for (let item of monSet) console.log(item);
-
-allBtn.addEventListener("click", () => {
-  
-});
+  fetch("http://localhost:5678/api/categories")
+  .then(response => {
+    console.log(response.ok)
+    return response.json();
+})
 
 
-objetBtn.addEventListener("click", () => {
+const displayCategory = (data) => {
+  const category = document.querySelector("#portfolio .btn")
+  for ( let item of data){
+    console.log(item.category)
+    const button =
+    ` <button>Tous</button>
+			<button class="objet">Objets</button>
+			<button class="appartement">Appartement</button>
+			<button class="hotelRestau">Hôtel & restaurants</button>`
+    category.insertAdjacentHTML('beforeend',button)
+  }
+}
 
-});
-console.log(objetBtn)
-
-appartementBtn.addEventListener("click", () => {
-  
-});
-
-hrBtn.addEventListener("click", () => {
-
-});
 
 
 
