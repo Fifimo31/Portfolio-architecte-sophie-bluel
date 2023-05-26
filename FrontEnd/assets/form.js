d9b16d249fe9dac0c5cd05fb90839c9577527bdf
Hawa Sall <sallhawa30@yahoo.fr>
2023-05-26 22:17:04 +0200

    continuité sur le fetch post

--- FrontEnd/assets/form.js
+++ FrontEnd/assets/form.js
@@ -11,27 +11,14 @@ submitButton.innerHTML = "";
 
 const forgotPasswordLink = document.querySelector("a") // lien de mot de passe oublié
  forgotPasswordLink.innerHTML = "";
+ const data = {
+  email: email,
+  password: login
+}
+console.log(data)
 
-const url = "http://localhost:5678/api/users/login";
-  const validForm = async(url, data) => {
-  
-  fetch(url, {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    method: "POST",
-    body: JSON.stringify(data)
-  })
-    .then((res) => {
-      console.log(res);})
-    
-      .catch((error) => {
-      console.log(error);});
-      displayData(data)
-};
 
-validForm(url)
+
 // Gestionnaire d'événement pour le bouton de soumission
 formAdmin.addEventListener("submit", (e) => {
   e.preventDefault(); // empêche la soumission du formulaire avant la transmission des informations demander 
@@ -43,18 +30,28 @@ formAdmin.addEventListener("submit", (e) => {
     email: email,
     password: login
   }
+
+  const url = "http://localhost:5678/api/users/login";
+  const validForm = async(url, data) => {
+  
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    
+    
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
+
   console.log(data)
   const response = validForm(url, data)
   console.log(response)
-  if (email === "sophie.bluel@test.tld" && login === "S0phie") {
-    window.location.href = "http://127.0.0.1:5501/FrontEnd/index.html";
-
-  } else {
-    const errorMessage = "Invalid email or password";
-    displayMessage(errorMessage);
-    console.log(errorMessage)
-  }
-
+  
 });
 
 
