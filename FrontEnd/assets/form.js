3a842ca83c3ad192c86dfbeff5b48671a942d502
Hawa Sall <sallhawa30@yahoo.fr>
2023-05-28 23:45:10 +0200

    Redirection vers la page d'acceuil

--- FrontEnd/assets/form.js
+++ FrontEnd/assets/form.js
@@ -1,24 +1,5 @@
 const formAdmin = document.querySelector("form");
 
-const emailField = document.querySelector("#email")// emailField = champ d'email
-emailField.innerHTML = ""; 
-
-const loginField = document.querySelector("#login")// loginField = champ de connexion
-loginField.innerHTML = "";
-
-const submitButton = document.querySelector("input[type='submit']") // (submit = soumettre)
-submitButton.innerHTML = "";
-
-const forgotPasswordLink = document.querySelector("a") // lien de mot de passe oublié
- forgotPasswordLink.innerHTML = "";
- const data = {
-  email: email,
-  password: login
-}
-console.log(data)
-
-
-
 // Gestionnaire d'événement pour le bouton de soumission
 formAdmin.addEventListener("submit", (e) => {
   e.preventDefault(); // empêche la soumission du formulaire avant la transmission des informations demander 
@@ -47,14 +28,22 @@ formAdmin.addEventListener("submit", (e) => {
     .then((response) => response.json())
     .then((json) => {
       console.log(json)
-      sessionStorage.setItem('token', json.token)
     });
+  
   }
 
   console.log(data)
   const response = validForm(url, data)
   console.log(response)
-  
-});
+
+  if (email === "sophie.bluel@test.tld" && login === "S0phie") {
+    window.location.href = "http://127.0.0.1:5501/FrontEnd/index.html";
+
+  } else {
+    const errorMessage = response;
+    displayMessage(errorMessage);
+    console.log(errorMessage)
+  }
+})
 
 
